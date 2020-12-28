780e19f4ca250747d805a301cb696b7e089baece
Jire <thomasgnappo@gmail.com>
2020-12-27 21:14:10 -0500

    test: added java test

---
+++ src/test/java/org/jire/datalayered/TestJava.java
@@ -0,0 +1,24 @@
+package org.jire.datalayered;
+
+import org.jire.datalayered.generated.datalayered;
+import org.jire.datalayered.generated.datalayered_members;
+
+public final class TestJava {
+	
+	public static void main(String[] args) {
+		datalayered datalayered = new datalayered();
+		datalayered.init();
+		
+		datalayered_members members = datalayered.members;
+		System.out.println("default UID " + members.uid.get(members.getDefaultKey()));
+		
+		long member1 = members.newKey();
+		System.out.println("member1=" + member1);
+		
+		members.uid.set(member1, 1337);
+		
+		long member1UID = members.uid.get(member1);
+		System.out.println("member1UID=" + member1UID);
+	}
+	
+}
